b03aa50c1c1e248793cae9e7a8148637e83fefdd
Noxser <noxser@gmail.com>
2018-05-24 16:22:33 +0500

    Начал работу

--- game.js
+++ game.js
@@ -1,1 +1,22 @@
 'use strict';
+
+class Vector {
+    
+    constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    plus(vector) {
+        if (vector instanceof Vector) {
+            return new Vector(this.x + vector.x, this.y + vector.y);
+        }
+        throw new Error("Можно прибавлять к вектору только вектор типа Vector");
+    }
+    
+    
+    
+            
+}
+    
+
